b9dbeaeffb3f196712e8ff658c80a1d2afe4fe2f
sauce-Git <dadad1004@naver.com>
2024-03-09 12:44:55 +0900

    Add checkRole service and its unit tests

---
+++ src/services/checkRole.service.spec.ts
@@ -0,0 +1,114 @@
+import { TestingModule, Test } from '@nestjs/testing';
+import { PrismaClient, access_role, folders, user_role } from '@prisma/client';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { CheckRoleService } from './checkRole.service';
+import { PrismaService } from './prisma.service';
+import { v4 as uuidv4 } from 'uuid';
+import { NotFoundException } from '@nestjs/common';
+
+describe('CheckRoleService', () => {
+  let service: CheckRoleService;
+  let prismaService: DeepMockProxy<PrismaClient>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CheckRoleService, PrismaService],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep(PrismaClient))
+      .compile();
+
+    service = module.get<CheckRoleService>(CheckRoleService);
+    prismaService = module.get<DeepMockProxy<PrismaClient>>(PrismaService);
+  });
+
+  // Test if the service is defined
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  /**
+   * Success handling
+   * Test if the service is successfully done
+   */
+
+  // Check role success handling
+  it('should return true if user have role', async () => {
+    const folderKey = uuidv4();
+    const userId = 1;
+    const role: access_role = 'create';
+    const folder: folders = {
+      id: BigInt(1),
+      folder_name: 'test',
+      parent_folder_id: null,
+      folder_key: folderKey,
+    };
+    const userRole: user_role = {
+      user_id: userId,
+      folder_id: folder.id,
+      role: ['create', 'update'],
+    };
+    prismaService.folders.findUnique.mockResolvedValue(folder);
+    prismaService.user_role.findFirst.mockResolvedValue(userRole);
+
+    const result = await service.checkRole(folderKey, userId, role);
+    expect(result).toBe(true);
+  });
+
+  it('should return false if user does not have role', async () => {
+    const folderKey = uuidv4();
+    const userId = 1;
+    const role: access_role = 'delete';
+    const folder: folders = {
+      id: BigInt(1),
+      folder_name: 'test',
+      parent_folder_id: null,
+      folder_key: folderKey,
+    };
+    const userRole: user_role = {
+      user_id: userId,
+      folder_id: folder.id,
+      role: ['create', 'update'],
+    };
+    prismaService.folders.findUnique.mockResolvedValue(folder);
+    prismaService.user_role.findFirst.mockResolvedValue(userRole);
+
+    const result = await service.checkRole(folderKey, userId, role);
+    expect(result).toBe(false);
+  });
+
+  /**
+   * Failure handling
+   * Test if the service is failed
+   */
+
+  // Check role failure handling
+  it('should throw NotFoundException if folder does not exist', async () => {
+    const folderKey = uuidv4();
+    const userId = 1;
+    const role = 'create';
+    prismaService.folders.findUnique.mockResolvedValue(null);
+
+    await expect(service.checkRole(folderKey, userId, role)).rejects.toThrow(
+      NotFoundException,
+    );
+  });
+
+  it('should throw NotFoundException if user_role does not exist', async () => {
+    const folderKey = uuidv4();
+    const userId = 1;
+    const role = 'create';
+    const folder: folders = {
+      id: BigInt(1),
+      folder_name: 'test',
+      parent_folder_id: null,
+      folder_key: folderKey,
+    };
+    prismaService.folders.findUnique.mockResolvedValue(folder);
+    prismaService.user_role.findFirst.mockResolvedValue(null);
+
+    await expect(service.checkRole(folderKey, userId, role)).rejects.toThrow(
+      NotFoundException,
+    );
+  });
+});
--- src/services/checkRole.service.ts
+++ src/services/checkRole.service.ts
@@ -1,6 +1,34 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { access_role } from '@prisma/client';
 
 @Injectable()
 export class CheckRoleService {
-  
+  constructor(private prisma: PrismaService) {}
+
+  async checkRole(folderKey: string, userId: number, role: access_role) {
+    const folder = await this.prisma.folders.findUnique({
+      where: {
+        folder_key: folderKey,
+      },
+    });
+    if (!folder) {
+      throw new NotFoundException('Folder does not exist');
+    }
+
+    const userRole = await this.prisma.user_role.findFirst({
+      where: {
+        folder_id: folder.id,
+        user_id: userId,
+      },
+    });
+    if (!userRole) {
+      throw new NotFoundException('User does not have access to the folder');
+    }
+
+    // Check if the user has the role
+    const hadRole = userRole.role.includes(role);
+
+    return hadRole;
+  }
 }
