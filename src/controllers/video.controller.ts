ce05aa376fa258285f88b780d393d2fbba57ed28
sauce-Git <dadad1004@naver.com>
2024-06-03 23:08:26 +0900

    chore: Add video streaming endpoint and update dependencies

--- src/controllers/video.controller.ts
+++ src/controllers/video.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Header, HttpCode, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { access_role } from '@prisma/client';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { RoleGuard } from 'src/guards/role.guard';
@@ -6,16 +13,25 @@ import { UserGuard } from 'src/guards/user.guard';
 import fs from 'fs';
 
 @Controller('videos')
-// @UseGuards(AuthGuard, UserGuard)
+@UseGuards(AuthGuard, UserGuard)
 export class VideoController {
   constructor() {}
 
-  @Get('/html/stream')
-  // @UseGuards(RoleGuard(access_role.read))
+  // @Get('/html/stream')
+  // @HttpCode(200)
+  // @Header('Content-Type', 'text/html')
+  // async streamVideo(): Promise<string> {
+  //   const playerHtml = fs.readFileSync('src/utils/player.html', 'utf8');
+  //   return playerHtml;
+  // }
+
+  @Get('/stream/:folderKey/:fileKey')
+  @UseGuards(RoleGuard(access_role.read))
   @HttpCode(200)
-  @Header('Content-Type', 'text/html')
-  async streamVideo(): Promise<string> {
-    const playerHtml = fs.readFileSync('src/utils/player.html', 'utf8');
-    return playerHtml;
+  @Header('Content-Type', 'video/mp4')
+  async streamVideoFile(@Query('path') path: string): Promise<fs.ReadStream> {
+    const videoPath = `test/sample/video/${path}`;
+    const videoStream = fs.createReadStream(videoPath);
+    return videoStream;
   }
 }
