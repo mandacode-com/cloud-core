4fc41fc08bae1bcb1004b10d5f81553317afdcb5
sauce-Git <dadad1004@naver.com>
2024-06-04 15:12:50 +0900

    Remove static serve code and jwt service

--- src/modules/video.module.ts
+++ src/modules/video.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { VideoController } from 'src/controllers/video.controller';
 import { CheckRoleService } from 'src/services/checkRole.service';
 import { PrismaService } from 'src/services/prisma.service';
 import { UserService } from 'src/services/user.service';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: 'test/sample/video',
-      serveRoot: '/videos/stream',
-    }),
-  ],
-  providers: [CheckRoleService, PrismaService, UserService],
   controllers: [VideoController],
+  providers: [UserService, PrismaService, CheckRoleService],
 })
 export class VideoModule {}
