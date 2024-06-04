f925b87e883a245f0c8446e4fe4b68cfade58335
sauce-Git <dadad1004@naver.com>
2024-06-04 15:13:52 +0900

    Created createStreamVideoFile for testing in e2e setup file

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -216,7 +216,7 @@ export const createFile = async (
     `INSERT INTO "cloud"."file_info" (file_id, uploader_id, byte_size) VALUES (${fileId}, ${userId}, ${byteSize}) RETURNING *`,
   );
   if (fileBuffer) {
-    const baseDir = process.env.FILE_UPLOAD_DIR || 'uploads';
+    const baseDir = process.env.BASE_STORAGE_PATH || 'storage';
     const extName = path.extname(fileName);
     if (!fs.existsSync(baseDir)) {
       fs.mkdirSync(baseDir);
@@ -227,6 +227,34 @@ export const createFile = async (
       fileBuffer,
     );
   }
+  return {
+    file: file.rows[0],
+    fileInfo: fileInfo.rows[0],
+  };
+};
+
+export const createStreamVideoFile = async (
+  userId: number,
+  fileId: bigint,
+  parentFolderId: bigint,
+  fileName: string,
+  enabled: boolean = true,
+  byteSize: number = 100,
+) => {
+  const fileKey = uuidv4();
+  const file = await postgresClient.query<files, files[]>(
+    `INSERT INTO "cloud"."files" (id, parent_folder_id, file_name, enabled, file_key) VALUES (${fileId}, ${parentFolderId}, '${fileName}', ${enabled}, '${fileKey}') RETURNING *`,
+  );
+  const fileInfo = await postgresClient.query<file_info, file_info[]>(
+    `INSERT INTO "cloud"."file_info" (file_id, uploader_id, byte_size) VALUES (${fileId}, ${userId}, ${byteSize}) RETURNING *`,
+  );
+  const baseDir = process.env.BASE_STORAGE_PATH || 'storage';
+  if (!fs.existsSync(baseDir)) {
+    fs.mkdirSync(baseDir);
+  }
+  fs.cpSync(`${__dirname}/sample/video`, `${baseDir}/video/${fileKey}`, {
+    recursive: true,
+  });
   return {
     file: file.rows[0],
     fileInfo: fileInfo.rows[0],
