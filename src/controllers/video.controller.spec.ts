f026c4644bd42a59bb6765b7da0a08231e12d1ca
sauce-Git <dadad1004@naver.com>
2024-06-04 15:12:18 +0900

    Create method to stream a video and its test code

---
+++ src/controllers/video.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { VideoController } from './video.controller';
+import { UserService } from 'src/services/user.service';
+import { CheckRoleService } from 'src/services/checkRole.service';
+import { PrismaService } from 'src/services/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+
+describe('VideoController', () => {
+  let controller: VideoController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [VideoController],
+      providers: [UserService, PrismaService, CheckRoleService, JwtService],
+    }).compile();
+    controller = module.get<VideoController>(VideoController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
--- src/controllers/video.controller.ts
+++ src/controllers/video.controller.ts
@@ -3,7 +3,10 @@ import {
   Get,
   Header,
   HttpCode,
-  Query,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { access_role } from '@prisma/client';
@@ -11,27 +14,45 @@ import { AuthGuard } from 'src/guards/auth.guard';
 import { RoleGuard } from 'src/guards/role.guard';
 import { UserGuard } from 'src/guards/user.guard';
 import fs from 'fs';
+import { Response } from 'express';
+import { storagePath } from 'src/utils/storagePath';
+import { TypiaValidationPipe } from 'src/pipes/validation.pipe';
+import { validateResolution } from 'src/interfaces/file.interface';
 
 @Controller('videos')
 @UseGuards(AuthGuard, UserGuard)
 export class VideoController {
   constructor() {}
-
-  // @Get('/html/stream')
-  // @HttpCode(200)
-  // @Header('Content-Type', 'text/html')
-  // async streamVideo(): Promise<string> {
-  //   const playerHtml = fs.readFileSync('src/utils/player.html', 'utf8');
-  //   return playerHtml;
-  // }
-
-  @Get('/stream/:folderKey/:fileKey')
+  @Get('/stream/:folderKey/:fileKey/master.m3u8')
   @UseGuards(RoleGuard(access_role.read))
   @HttpCode(200)
-  @Header('Content-Type', 'video/mp4')
-  async streamVideoFile(@Query('path') path: string): Promise<fs.ReadStream> {
-    const videoPath = `test/sample/video/${path}`;
+  @Header('Content-Type', 'application/x-mpegURL')
+  async streamMasterPlaylist(
+    @Param('fileKey', new ParseUUIDPipe()) fileKey: string,
+    @Res() res: Response,
+  ): Promise<void> {
+    const videoDir = storagePath.videoDir;
+    const videoPath = `${videoDir}/${fileKey}/master.m3u8`;
+    const masterPlaylist = fs.readFileSync(videoPath, 'utf8');
+    res.send(masterPlaylist);
+  }
+
+  @Get('stream/:folderKey/:fileKey/:resolution/:fileName')
+  @UseGuards(RoleGuard(access_role.read))
+  @HttpCode(200)
+  async streamVideoFile(
+    @Param('fileKey', new ParseUUIDPipe()) fileKey: string,
+    @Param('resolution', new TypiaValidationPipe(validateResolution))
+    resolution: string,
+    @Param('fileName') fileName: string,
+    @Res() res: Response,
+  ): Promise<void> {
+    const videoDir = storagePath.videoDir;
+    const videoPath = `${videoDir}/${fileKey}/${resolution}/${fileName}`;
+    if (!fs.existsSync(videoPath)) {
+      throw new NotFoundException('Video not found');
+    }
     const videoStream = fs.createReadStream(videoPath);
-    return videoStream;
+    videoStream.pipe(res);
   }
 }
