9f1a116c17eafe3a110a8f4847cf53256c4bd9f5
sauce <dadad1004@naver.com>
2024-10-10 14:52:01 +0900

    fix: delete role guard
    Delete role guard which check role on the file that is not created yet

--- src/controllers/file/upload.controller.ts
+++ src/controllers/file/upload.controller.ts
@@ -4,7 +4,7 @@ import {
   HttpCode,
   Param,
   ParseIntPipe,
-  Post,
+  Patch,
   Query,
   UseGuards,
 } from '@nestjs/common';
@@ -50,12 +50,11 @@ export class UploadController {
     return response;
   }
 
-  @Post('complete/:fileKey')
+  @Patch('complete/:fileKey')
   @HttpCode(201)
-  @UseGuards(RoleGuard(access_role.create))
   async completeUpload(
     @Param('fileKey') fileKey: string,
-    @Query('totalChunks', ParseIntPipe) totalChunks: number,
+    @Query('total_chunks', ParseIntPipe) totalChunks: number,
   ) {
     const data = await this.uploadService.completeUpload(fileKey, totalChunks);
     const response: CustomResponse<{
