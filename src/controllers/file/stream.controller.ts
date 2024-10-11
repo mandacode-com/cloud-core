a3e3c8165e35fdc891fbe96c2264998551c72138
sauce <dadad1004@naver.com>
2024-10-11 11:55:32 +0900

    feat: connect stream module to app

---
+++ src/controllers/file/stream.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, HttpCode, Param, UseGuards } from '@nestjs/common';
+import { access_role } from '@prisma/client';
+import { MemberGuard } from 'src/guards/member.guard';
+import { RoleGuard } from 'src/guards/role.guard';
+import { StreamService } from 'src/services/stream.service';
+
+@Controller('file/stream')
+@UseGuards(MemberGuard)
+export class StreamController {
+  constructor(private readonly streamService: StreamService) {}
+
+  @Get('read-token/:fileKey')
+  @HttpCode(200)
+  @UseGuards(RoleGuard(access_role.read))
+  async issueReadToken(@Param('fileKey') fileKey: string) {
+    const token = await this.streamService.issueReadToken(fileKey);
+    return token;
+  }
+}
